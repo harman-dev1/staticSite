3fdb3ec7dbf70160746638c91a58f872b4737748
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2024-12-16 20:00:46 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,13 @@
+// pages/index.js
 import { getPostData } from '../lib/posts';
 
-export default function Home({ postData }) {
+export default function Home() {
+  const { data, content } = getPostData();
   return (
     <div>
-      <h1>{postData.data.title}</h1>
-      <p>{postData.data.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: postData.content }} />
+      <h1>{data.title}</h1>
+      <p>{data.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
-}
-
-// Fetch data server-side with getStaticProps
-export async function getStaticProps() {
-  const postData = getPostData(); // Call your server-side function
-  return {
-    props: {
-      postData, // Pass data to your component
-    },
-  };
 }
